a86f701bcaf4e74f95b6dc9d973c5f416a55e7e1
Davi Andrade <contatodaviandrade20@gmail.com>
2026-04-04 20:49:46 -0300

    Adiciona banco de palavras e lógica de sorteio aleatório

--- JogoTermo.ConsoleApp/Program.cs
+++ JogoTermo.ConsoleApp/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace JogoTermo.ConsoleApp;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JogoTermo.ConsoleApp;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // 1. Lista de palavras de 5 letras
+        List<string> bancoDePalavras = new List<string>
+        {
+            "PROVA", "SABER", "VIVER", "FORTE", "PONTO", 
+            "LUGAR", "FALAR", "MENTE", "NOITE", "PORTA", 
+            "PLANO", "BRISA", "CALOR", "MUNDO", "PRATO",
+            "TREINO", "CORTE", "GRITO", "RADIO", "MOSCA"
+        };
+
+        // Sorteia a palavra secreta
+        Random random = new Random();
+        string palavraSecreta = bancoDePalavras[random.Next(bancoDePalavras.Count)].ToUpper();
+
+        int limiteTentativas = 5;
+        int tentativaAtual = 0;
+        bool venceu = false;
     }
 }
