c5fa64b1ecf7d7fc2f08dba25c9aefbe54945ba0
Davi Andrade <contatodaviandrade20@gmail.com>
2026-04-05 15:39:34 -0300

    Adiciona interface inicial e validação de entrada do usuário

--- JogoTermo.ConsoleApp/Program.cs
+++ JogoTermo.ConsoleApp/Program.cs
@@ -14,7 +14,7 @@ class Program
             "PROVA", "SABER", "VIVER", "FORTE", "PONTO", 
             "LUGAR", "FALAR", "MENTE", "NOITE", "PORTA", 
             "PLANO", "BRISA", "CALOR", "MUNDO", "PRATO",
-            "TREINO", "CORTE", "GRITO", "RADIO", "MOSCA"
+            "TREINO", "LIVRO", "CAIXA", "RADIO", "CARRO"
         };
 
         // Sorteia a palavra secreta
@@ -24,5 +24,34 @@ class Program
         int limiteTentativas = 5;
         int tentativaAtual = 0;
         bool venceu = false;
-    }
+    
+    
+        // 3. Loop principal do jogo
+        while (tentativaAtual < limiteTentativas && !venceu)
+        {
+            Console.Clear();
+            Console.WriteLine("============================================");
+            Console.WriteLine("    BEM VINDO AO JOGO DE ADIVINHAÇÃO   ");
+            Console.WriteLine("============================================");
+            Console.WriteLine($"Tentativa {tentativaAtual + 1} de {limiteTentativas}");
+            Console.WriteLine("DICA: [Verde] correto | [Amarelo] Posição Errada | [Vermelho] Inexistente");
+            Console.WriteLine("============================================\n");
+
+            Console.Write("Digite seu palpite (5 letras): ");
+            string palpite = Console.ReadLine()!.ToUpper().Trim();
+
+            // Validação
+            if (palpite.Length != 5)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nERRO: A palavra deve ter exatamente 5 letras!");
+                Console.ResetColor();
+                Console.WriteLine("Pressione qualquer tecla para tentar novamente...");
+                Console.ReadKey();
+                continue; // Volta para o início do while sem gastar tentativa
+            }
+
+    
+        }
+    }   
 }
